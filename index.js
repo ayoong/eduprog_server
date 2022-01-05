91a0def4444f6e01eb0ff953a79311fd52566f03
malikkurosaki <kurosakiblackangel@gmail.com>
2022-01-05 23:14:04 +0800

    baru ris

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const expressAsyncHandler = require('express-async-handler');
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-const {PrismaClient} = require('@prisma/client');
+app.use(express.urlencoded({ extended: true }));
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 
@@ -13,8 +13,8 @@ app.post('/login', expressAsyncHandler(async (req, res) => {
     const dataLogin = req.body;
     const data = await prisma.users.findMany({
         where: {
-           user_name: dataLogin.user_name,
-           user_password: dataLogin.user_password
+            user_name: dataLogin.user_name,
+            user_password: dataLogin.user_password
         }
     });
 
@@ -23,8 +23,8 @@ app.post('/login', expressAsyncHandler(async (req, res) => {
     const ada = data.length > 0;
     res.json({
         success: ada,
-        data: ada? data[0]: null,
-        desc: ada?"login succcess": "wrong user or password"
+        data: ada ? data[0] : null,
+        desc: ada ? "login succcess" : "wrong user or password"
     });
 }))
 
@@ -34,9 +34,11 @@ app.get('/users', expressAsyncHandler(async (req, res) => {
     res.json(data);
 }));
 
+
+// untuk transaksi perharinya
 app.get('/transaksi/:tanggal', expressAsyncHandler(async (req, res) => {
     const tanggal = req.params.tanggal;
-    const data = await  prisma.transaksi2.findMany({
+    const data = await prisma.transaksi2.findMany({
         // skip: 0,
         // take: 200,
         // orderBy: {
@@ -58,6 +60,41 @@ app.get('/transaksi/:tanggal', expressAsyncHandler(async (req, res) => {
     res.json(data);
 }));
 
+
+// untuk transaksi perbulannya
+app.get('/transaksiPerBulan/:tanggal', expressAsyncHandler(async (req, res) => {
+    let tanggalAwal = new Date(req.params.tanggal).setDate(01);
+    let tanggalAkhir = new Date(req.params.tanggal).setDate(31);
+
+    var data = await prisma.transaksi2.groupBy({
+        by: ["tanggal"],
+        _count: {
+            netto_rekon: true
+        },
+        _sum: {
+            netto_rekon: true
+        },
+        where: {
+            tanggal: {
+                lte: new Date(tanggalAkhir),
+                gte: new Date(tanggalAwal)
+            },
+
+        }
+    });
+
+    if(data || data.length != 0){
+        data = data.map((e) => {
+            return {
+                tanggal: e.tanggal,
+                rit: e._count.netto_rekon,
+                tonase: e._sum.netto_rekon
+            }
+        })
+    }
+    res.json(data);
+}));
+
 app.get('/chart', expressAsyncHandler(async (req, res) => {
     let chart = await prisma.transaksi2.findMany({
         select: {
@@ -74,20 +111,20 @@ app.get('/chart', expressAsyncHandler(async (req, res) => {
             "jam": new Date(e.jam_in).getHours(),
             "net": e['netto_rekon']
         }
-    }).sort(function(a, b) {
+    }).sort(function (a, b) {
         return parseFloat(a.jam) - parseFloat(b.jam);
     });
 
-    var output = data.reduce(function(accumulator, cur) {
+    var output = data.reduce(function (accumulator, cur) {
         var jam = cur.jam;
-        var found = accumulator.find(function(elem) {
+        var found = accumulator.find(function (elem) {
             return elem.jam == jam
         });
 
         if (found) found.net += cur.net;
         else accumulator.push(cur);
         return accumulator;
-      }, []);
+    }, []);
 
 
     res.json(output)
@@ -116,31 +153,66 @@ app.get('/transaksiBulan/:tanggal', expressAsyncHandler(async (req, res) => {
             "tanggal": new Date(e.tanggal).getDate(),
             "net": e['netto_rekon']
         }
-    }).sort(function(a, b) {
+    }).sort(function (a, b) {
         return parseFloat(a.jam) - parseFloat(b.jam);
     });
 
-    var output = data.reduce(function(accumulator, cur) {
+    var output = data.reduce(function (accumulator, cur) {
         var tanggal = cur.tanggal;
-        var found = accumulator.find(function(elem) {
+        var found = accumulator.find(function (elem) {
             return elem.tanggal == tanggal
         });
 
         if (found) found.net += cur.net;
         else accumulator.push(cur);
         return accumulator;
-      }, []);
+    }, []);
 
 
     return res.json(output);
 }))
 
 
-app.get('/ritAndTonase', expressAsyncHandler(async (req, res) => {
+app.get('/ritAndTonase/:tanggal', expressAsyncHandler(async (req, res) => {
+    let awal = new Date(req.params.tanggal);
+    let akhir = awal.setDate(awal.getDate() + 1);
+
+
     let data = await prisma.transaksi2.aggregate({
         _count: true,
         _sum: {
             netto_rekon: true
+        },
+        where: {
+            tanggal: {
+                lte: new Date(akhir),
+                gte: awal
+            }
+        }
+    });
+
+    res.json({
+        "rit": data['_count'],
+        "tonase": data['_sum']['netto_rekon']
+    });
+}))
+
+app.get('/ritTonasePerBulan/:tanggal', expressAsyncHandler(async (req, res) => {
+    let awal = new Date(req.params.tanggal).setDate(1);
+    let akhir = new Date(req.params.tanggal).setDate(31);
+
+    let data = await prisma.transaksi2.aggregate({
+        _count: true,
+        _sum: {
+            netto_rekon: true
+        },
+        where: {
+            tanggal: {
+                // lower than or equal => lebih kecil dari atau sama dengan
+                lte: new Date(akhir),
+                // grather than or equal => lebih besar atau sama dengan
+                gte: new Date(awal)
+            }
         }
     });
 
---
+++ xmakuro.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MENU="
+Pilihan Menu: 
+1. jalankan server
+2. git push
+3. git pull
+"
